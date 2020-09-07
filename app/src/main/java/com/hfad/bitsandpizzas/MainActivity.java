5d11d665d70922312371cb86e7fe0cedb1bb2151
quangcanh2975 <ubuntultc@gmail.com>
2020-09-08 06:14:49 +0700

    Added icon and Order Activity; Icon still not display in Menu

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,7 +1,12 @@
 package com.hfad.bitsandpizzas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_setting:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
