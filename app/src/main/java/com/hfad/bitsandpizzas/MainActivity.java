9699d2548f31e56ae8c290a797a1337d8c3b4026
quangcanh2975 <ubuntultc@gmail.com>
2020-09-09 06:12:09 +0700

    Adding function to select Drawer list item

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,25 +1,60 @@
 package com.hfad.bitsandpizzas;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.MenuItemCompat;
 import androidx.appcompat.widget.ShareActionProvider;
-;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private ShareActionProvider mShareActionProvider;
+    private String[] titles;
+    private ListView drawList;
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Log.d("Pizzas", "clicked");
+        }
+        private void selectItem(int position){
+            Fragment fragment;
+            switch (position){
+                case 1:
+                   fragment = new PizzaFragment();
+                   break;
+
+                case 2:
+                    fragment = new TopFragment();
+                    break;
+                case 3:
+                    fragment = new StoresFragment();
+                    break;
+                default:
+                    fragment =
+                            break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titles = getResources().getStringArray(R.array.titles);
+        drawList = (ListView) findViewById(R.id.drawer);
+        drawList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, titles));
+        drawList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
     @Override
--- app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -1,13 +1,21 @@
 package com.hfad.bitsandpizzas;
 
-import android.app.Activity;
+import android.app.ActionBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class OrderActivity extends Activity {
+public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+//        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
